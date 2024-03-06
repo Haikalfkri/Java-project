ef47dad9aa693492fa3cd0b6a410bf9d84fd7955
Haikal Fikri <haikaljr1245@gmail.com>
2024-03-06 23:07:26 +0700

    add simple calculator

---
+++ simpleCalculator.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+/**
+ * simpleCalculator
+ */
+public class simpleCalculator {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        boolean next = true;
+
+        while (next) {
+            System.out.print("Enter your first number: ");
+            double number1 = scanner.nextDouble();
+
+            System.out.print("Enter you second number: ");
+            double number2 = scanner.nextDouble();
+
+            scanner.nextLine();
+
+            System.out.print("Choose your operator (+, -, *, /): ");
+            String operator = scanner.nextLine();
+
+            double result;
+
+            switch (operator) {
+                case "+":
+                    result = number1 + number2;
+                    System.out.println("Result: " + result);
+                    break;
+                case "-":
+                    result = number1 -number2;
+                    System.out.println("Result: " + result);
+                    break;
+                case "*":
+                    result = number1 * number2;
+                    System.out.println("Result: " + result);
+                    break;
+                case "/":
+                    result = number1 / number2;
+                    System.out.println("Result: " + result);
+                    break;
+                default:
+                    System.out.println("Invalid operator");
+            }   
+        
+            System.out.print("Continue? (yes/no): ");
+            String cont = scanner.nextLine().toLowerCase();
+        
+            if (cont.equals("yes")) {
+                continue;
+            } else {
+                break;
+            }
+        }
+
+    }
+}
