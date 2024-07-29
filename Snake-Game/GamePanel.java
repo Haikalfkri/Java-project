55c145acc27de62072ddc84e698f5d3acc970e6d
Haikal Fikri <haikaljr1245@gmail.com>
2024-07-30 06:34:54 +0700

    Email Administration App

---
+++ Snake-Game/GamePanel.java
@@ -0,0 +1,16 @@
+import java.awt.event.ActionEvent;
+import java.aws.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel() {
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
