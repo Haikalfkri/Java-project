12072572d6d15c2b9d0082f5c815b4fa9d5bcb55
Haikal Fikri <haikaljr1245@gmail.com>
2024-08-12 05:44:03 +0700

    School Management System

---
+++ School-Management-System/src/Student.java
@@ -0,0 +1,25 @@
+public class Student {
+
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
+    public Student(int id, String name, int grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+        this.feesPaid = 0;
+        this.feesTotal = 30000;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public void updateFeesPaid(int fees) {
+        feesPaid += fees;
+    }
+
+}
